37f15926dc6be68cd447a3d9b1e0bcb8196e0594
Manh Vu <vndmanh@tma.com.vn>
2024-08-30 09:43:56 +0700

    Initial Commit

---
+++ SwiftLintIntergration/SwiftLintIntergrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLintIntergrationApp.swift
+//  SwiftLintIntergration
+//
+//  Created by vndmanh on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLintIntergrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
